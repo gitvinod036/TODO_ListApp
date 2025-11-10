6f49440fe9ce4e736c8e41b610a6354c303e817c
gitvinod036 <sannapanenivinod036@gmail.com>
2025-11-10 16:01:47 +0530

    Added Ignore

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -2,6 +2,9 @@ import { Todos } from "./components/Todo"
 
 function App() {
   return <>
+  <meter value="0.6" min="0" max="1">50%</meter>
+  <progress value={0.6} min='0' max='1'>80%</progress>
+  <button aria-label="Close modal">✖</button>
   <Todos/>
   </>
 }
